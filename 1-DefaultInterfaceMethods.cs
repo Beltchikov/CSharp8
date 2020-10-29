9e474e7e2e811f76c21f0bdd7a21706deeb9726b
Jogger <beltchikov@gmx.de>
2020-10-29 18:02:27 +0100

    Default interface methods

---
+++ 1-DefaultInterfaceMethods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp8
+{
+    public interface IShoppingCart
+    {
+        void CalculateTotal();
+
+        // Second method breaks class implementation
+        void CalculateSubTotal()
+        // To avoid the breaking default implementition can be used
+        {
+
+        }
+    }
+
+    public class ShoppingCart : IShoppingCart
+    {
+        public void CalculateTotal()
+        {
+            Console.WriteLine("CalculateTotal()");
+        }
+    }
+}
