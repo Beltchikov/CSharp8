7bb0d36293c8810cac79022bf8b7f77ca91ce996
Jogger <beltchikov@gmx.de>
2020-10-30 09:33:45 +0100

    Usinng declarations

---
+++ 3-UsingDeclarations.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CSharp8
+{
+    public class UsingDeclarations
+    {
+        public static void ConvertFiles()
+        {
+            bool oldWay = false;
+
+            if (oldWay)
+            {
+                using (var inputFile = new StreamReader(@"C:\Temp\input.txt"))
+                {
+                    using (var ouputFile = new StreamWriter(@"C:\Temp\output.txt"))
+                    {
+                        string line;
+                        while ((line = inputFile.ReadLine()) != null)
+                        {
+                            ouputFile.WriteLine(line);
+                        }
+                    }
+                }
+            }
+            else // New way
+            {
+                using var inputFile = new StreamReader(@"C:\Temp\input.txt");
+                using var outputFile = new StreamWriter(@"C:\Temp\output.txt");
+                string line;
+                while ((line = inputFile.ReadLine()) != null)
+                {
+                    outputFile.WriteLine(line);
+                }
+            }
+        }
+    }
+}
