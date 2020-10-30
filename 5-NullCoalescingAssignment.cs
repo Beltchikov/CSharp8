39bbfeeab6eee707fdadd46a07abe5c9802cd7a2
Jogger <beltchikov@gmx.de>
2020-10-30 10:42:58 +0100

    Null coalescing assingment

---
+++ 5-NullCoalescingAssignment.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp8
+{
+    public class NullCoalescingAssignment
+    {
+        public static void Demo()
+        {
+            bool oldWay = false;
+            var wrapper = new Wrapper();
+
+            if (oldWay)
+            {
+                if (wrapper.Numbers == null)
+                {
+                    wrapper.Numbers = new List<int>();
+                }
+            }
+            else // New way
+            {
+                wrapper.Numbers ??= new List<int>();
+            }
+        }
+    }
+
+    public class Wrapper
+    {
+        public List<int> Numbers { get; set; }
+    }
+}
