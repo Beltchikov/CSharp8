05079b30cab69193686183e07aa5b4045f08d92d
Jogger <beltchikov@gmx.de>
2020-11-13 09:41:03 +0100

    Nullable Reference Types

---
+++ 6-NullableReferenceTypes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp8
+{
+    public class NullableReferenceTypes
+    {
+        public static void Demo()
+        {
+            // Provoke NPE
+            string text = null;
+            var length = text.Length;
+
+            // Configure project file
+            string newConfigurationBlock = "<LangVersion>8</LangVersion>";
+            newConfigurationBlock += " < Nullable > enable </ Nullable >";
+        }
+    }
+}
